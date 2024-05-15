d8e0cf38a9137c6e8c78b20cc6266edd96a576e5
Dmitry <bog22232@ya.ru>
2024-05-15 11:44:33 +0400

    Initialize module

---
+++ IResourceLoader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ResourceLoader
+{
+public interface IResourceLoader : IDisposable
+{
+    public Task PreloadInCashAsync<TResource>(string resourceId, CancellationToken token);
+    public TResource LoadResource<TResource>(string resourceId);
+    public void LoadResource<TResource>(string resourceId, Action<TResource> onResourceLoaded, CancellationToken token);
+    public Task<TResource> LoadResourceAsync<TResource>(string resourceId, CancellationToken token);
+    public void ReleaseResource<TResource>(TResource resource);
+    public void ReleaseAllResources();
+}
+}
